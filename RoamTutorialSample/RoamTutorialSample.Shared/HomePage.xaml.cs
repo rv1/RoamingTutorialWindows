0cdac2f0b2821797826530fc4c63a325f447658b
VermaRahul <vermasonr@gmail.com>
2014-08-11 20:18:20 -0400

    Updated .gitignore and added template project and viewmodel

---
+++ RoamTutorialSample/RoamTutorialSample.Shared/HomePage.xaml.cs
@@ -0,0 +1,44 @@
+﻿using RoamTutorialSample.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace RoamTutorialSample
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class HomePage : Page
+    {
+        public HomePage()
+        {
+            this.DataContext = new RoamViewModel();
+            this.InitializeComponent();
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            var a = 1;
+            // TODO: Prepare page for display here.
+
+            // TODO: If your application contains multiple pages, ensure that you are
+            // handling the hardware Back button by registering for the
+            // Windows.Phone.UI.Input.HardwareButtons.BackPressed event.
+            // If you are using the NavigationHelper provided by some templates,
+            // this event is handled for you.
+        }
+    }
+}
---
+++ RoamTutorialSample/RoamTutorialSample.Shared/ViewModels/RoamViewModel.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+using Windows.ApplicationModel;
+
+namespace RoamTutorialSample.ViewModels
+{
+    class BindableBase : INotifyPropertyChanged
+    {
+        protected bool SetProperty<T>(ref T storage, T value,
+          [CallerMemberName] String propertyName = null)
+        {
+            if (object.Equals(storage, value)) return false;
+
+            storage = value;
+            this.OnPropertyChanged(propertyName);
+            return true;
+        }
+
+        protected void OnPropertyChanged(
+          [CallerMemberName] string propertyName = null)
+        {
+            var eventHandler = this.PropertyChanged;
+            if (eventHandler != null)
+            {
+                eventHandler(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+
+
+    class RoamViewModel : BindableBase
+    {
+        public string SelectedValue
+        {
+            get { return (this._selectedValue); }
+            set { base.SetProperty(ref this._selectedValue, value); }
+        }
+
+        string _selectedValue;
+
+        IEnumerable<string> images;
+
+        public IEnumerable<string> Images
+        {
+            get
+            {
+                if (this.images == null)
+                {
+                    GetImagesAsync();
+                }
+                return (images);
+            }
+        }
+
+        async Task GetImagesAsync()
+        {
+            // might want to cache this list perhaps.  
+            var folder = await Package.Current.InstalledLocation.GetFolderAsync("Images");
+
+            var files = await folder.GetFilesAsync();
+            this.images = files
+              .Where(f => System.IO.Path.GetExtension(f.Path) == @".png")
+              .Select(f => f.Path)
+              .ToList();
+
+            this.OnPropertyChanged("Images");
+        }
+    }
+}
